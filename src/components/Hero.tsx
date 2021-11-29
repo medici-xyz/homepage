b3f103a59a27a91af900a5761d2b78f02a03a869
Dmarz <danmarzec@icloud.com>
2021-11-29 00:36:28 -0500

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero: React.FC<{}> = () => {
   return (
     <section className="text-text-primary flex flex-col align-center justify-center flex-1">
       <h1 className="font-bold text-6xl text-center mb-4 animate-reveal-from-bottom">
-        "To every age its art, to every art its freedom"
+        NFTs are here, they're just not usable
       </h1>
       <h6 className="text-text-secondary text-center text-xl animate-reveal-from-bottom-delayed ">
         Supporting NFT usability.
